c58f3c449fd76f2b09b30cac4f8a351cce6296e4
prastowoadi <sinaadi75@gmail.com>
2022-08-19 16:46:18 +0700

    Create component widget and styling

---
+++ src/components/widget/Widget.jsx
@@ -0,0 +1,93 @@
+import "./widget.scss";
+import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import {
+  AccountBalanceOutlined,
+  MonetizationOnOutlined,
+  ShoppingCartOutlined,
+} from "@mui/icons-material";
+
+const Widget = ({ type }) => {
+  let data;
+
+  const amount = 100;
+  const diff = 20;
+
+  switch (type) {
+    case "user":
+      data = {
+        title: "USERS",
+        isMoney: false,
+        link: "See all users",
+        icon: (
+          <PersonOutlineOutlinedIcon
+            className="icon"
+            style={{ backgroundColor: "#fde047", color: "#ca8a04" }}
+          />
+        ),
+      };
+      break;
+    case "order":
+      data = {
+        title: "ORDERS",
+        isMoney: false,
+        link: "View all orders",
+        icon: (
+          <ShoppingCartOutlined
+            className="icon"
+            style={{ backgroundColor: "#bef264", color: "#65a30d" }}
+          />
+        ),
+      };
+      break;
+    case "earning":
+      data = {
+        title: "EARNINGS",
+        isMoney: true,
+        link: "View net earnings",
+        icon: (
+          <MonetizationOnOutlined
+            className="icon"
+            style={{ backgroundColor: "#67e8f9", color: "#0891b2" }}
+          />
+        ),
+      };
+      break;
+    case "balance":
+      data = {
+        title: "BALANCE",
+        isMoney: true,
+        link: "See details",
+        icon: (
+          <AccountBalanceOutlined
+            className="icon"
+            style={{ backgroundColor: "#c4b5fd", color: "#7c3aed" }}
+          />
+        ),
+      };
+      break;
+
+    default:
+      break;
+  }
+  return (
+    <div className="widget">
+      <div className="left">
+        <span className="title">{data.title}</span>
+        <span className="counter">
+          {data.isMoney && "$"} {amount}
+        </span>
+        <span className="link">{data.link}</span>
+      </div>
+      <div className="right">
+        <div className="percentage positive">
+          <KeyboardArrowUpOutlinedIcon />
+          {diff}%
+        </div>
+        {data.icon}
+      </div>
+    </div>
+  );
+};
+
+export default Widget;
