de3b97fb0aa682197ca8471b3d4f6695f4e48ef2
prastowoadi <sinaadi75@gmail.com>
2022-08-20 23:56:03 +0700

    Create datatable and single page omponent and styling it

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,9 +1,54 @@
+import Sidebar from "../../components/sidebar/Sidebar";
+import Chart from "../../components/chart/Chart";
+import Navbar from "../../components/navbar/Navbar";
 import "./single.scss";
-
+import List from "../../components/table/Table";
 const Single = () => {
   return (
     <div className="single">
-      <h1 className="title">Single</h1>
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+        <div className="top">
+          <div className="left">
+            <div className="editButton">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img
+                src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+                alt=""
+                className="itemImg"
+              />
+              <div className="details">
+                <div className="itemTitle">Veela</div>
+                <div className="detailItem">
+                  <span className="itemKey">Email:</span>
+                  <span className="itemValue">veela@gmail.com</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone:</span>
+                  <span className="itemValue">+6282 5678 9999</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Address:</span>
+                  <span className="itemValue">Grogol, Petamburan</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Country:</span>
+                  <span className="itemValue">Indonesia</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <Chart aspect={3 / 1} title="User spending (Last 6 Month)" />
+          </div>
+        </div>
+        <div className="bottom">
+          <h1 className="title">Last Transactions</h1>
+          <List />
+        </div>
+      </div>
     </div>
   );
 };
